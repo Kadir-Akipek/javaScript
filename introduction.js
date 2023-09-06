aaeb3bc26ae4b0a30304a9ce9a20b672e353bfaf
Kadir-Akipek <110154102+Kadir-Akipek@users.noreply.github.com>
2023-09-06 15:36:57 +0300

    Create introduction.js

---
+++ introduction.js
@@ -0,0 +1,24 @@
+console.log("Hello World!")
+console.log(3+4)
+
+//Data Types => String, İnt, Float, Booleans, Undefinied, Null
+
+console.log(typeof 'Asabeneh') // string
+console.log(typeof 5) // number
+console.log(typeof true) // boolean
+console.log(typeof null) // object type
+console.log(typeof undefined) // undefined(if we don't assign a value to a variable, the value is undefined.)
+
+//variables
+
+let age = 20 //For a variable that changes at a different time, we use let
+const PI = 3.14 //If the data does not change at all, we use const
+$name = "Kadir"
+_surname = "Akipek"
+
+let a //without assigning a data
+
+console.log(age, PI, $name, _surname)
+
+let name = 'Mustafa', job = 'Software engineer', live = 'Turkey' // Variables can also be declaring in one line separated by comma, however I recommend to use a seperate line to make code more readble
+console.log(name, job, live)
