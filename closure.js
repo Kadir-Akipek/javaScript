f73804bab3a9b5d7abbe4e545251e877a2a5e500
Kadir-Akipek <110154102+Kadir-Akipek@users.noreply.github.com>
2023-09-27 19:28:35 +0300

    update

---
+++ closure.js
@@ -0,0 +1,19 @@
+/*Closure
+JavaScript allows writing function inside an outer function. 
+We can write as many inner functions as we want. If inner 
+function access the variables of outer function then it is called closure.*/
+
+function outerFunction() {
+    let count = 0;
+    function innerFunction() {
+        count++
+        return count
+    }
+
+    return innerFunction
+}
+const innerFunc = outerFunction()
+
+console.log(innerFunc())
+console.log(innerFunc())
+console.log(innerFunc())
---
+++ json.js
@@ -0,0 +1,27 @@
+let sampleJSON = `{
+    "users":[
+      {
+        "firstName":"Asabeneh",
+        "lastName":"Yetayeh",
+        "age":250,
+        "email":"asab@asb.com"
+      },
+      {
+        "firstName":"Alex",
+        "lastName":"James",
+        "age":25,
+        "email":"alex@alex.com"
+      },
+      {
+      "firstName":"Lidiya",
+      "lastName":"Tekle",
+      "age":28,
+      "email":"lidiya@lidiya.com"
+      }
+    ]
+}`
+
+let users = JSON.parse(sampleJSON) //JSON convert to object
+console.log(JSON.stringify(users)) //object convert to JSON
+
+console.log(JSON.stringify(users, ["count", "name"], 4))
